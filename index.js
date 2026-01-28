49d6adf048839b20303222904eff6256ba9a2104
VIGNESH_YOHO <vigneshayyappan.yoho@gmail.com>
2026-01-28 12:07:06 +0530

    completed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://pastebin-2-0.vercel.app/"]
+    origin: ["http://localhost:5173", "https://pastebin-2-0.vercel.app" ]
   })
 );
 
